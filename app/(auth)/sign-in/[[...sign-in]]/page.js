da1e3137dc3fedf96323edc8195e5de6f1c541db
tedda980 <tewodrosgselase2424@gmail.com>
2024-10-12 19:52:18 +0300

    ...

--- app/(auth)/sign-in/[[...sign-in]]/page.js
+++ app/(auth)/sign-in/[[...sign-in]]/page.js
@@ -1,4 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Page() {
   return (
@@ -56,7 +57,7 @@ export default function Page() {
               </h1>
 
               <p className="mt-4 leading-relaxed text-gray-500">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Eligendi nam dolorum aliquam, quibusdam aperiam voluptatum.
               </p>
             </div>
